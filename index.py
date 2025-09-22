7a924bdcf1644ecf1e1206e1c3233f691ea251d0
SarojSur <rathindra101@gamil.com>
2025-09-22 21:57:02 +0600

    second

--- index.py
+++ index.py
@@ -2,3 +2,16 @@ print("hello world")
 print("hello codingal")
 print("25155+151")
 print(12*9269)
+print(22+2210392302789)
+print(2512500*420016+212552)
+
+
+print("hello world")
+print("hello codingal")
+print("25155+151")
+print(12*9269)
+print(22+2210392302789)
+print(2512500*420016+212552)
+
+
+
